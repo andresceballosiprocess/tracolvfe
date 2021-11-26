f68a4b5eccaf87f1249f8c020c73551da871acc9
andresceballosiprocess <69975801+andresceballosiprocess@users.noreply.github.com>
2021-11-26 17:11:31 -0500

    nov28

--- src/service/apiActa/apiActa.js
+++ src/service/apiActa/apiActa.js
@@ -5,7 +5,7 @@ export const getDatosActa=async({numeroTRA, codigoVerificacion})=>{
     console.log(numeroTRA)
     console.log(codigoVerificacion)
 
-    const datosActa=await axios.post('http://localhost:4000/acta',{numeroTRA,codigoVerificacion})
+    const datosActa=await axios.post('https://tracolverificacionserver.herokuapp.com/',{numeroTRA,codigoVerificacion})
     
     return await datosActa.data
     //.then((res)=>{
